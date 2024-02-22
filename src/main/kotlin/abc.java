ef5ac9d876d2094ac3f89a80cf49a24646875609
guan.pengjie <guan.pengjie@byd.com>
2024-02-22 16:24:27 +0800

    aaa

---
+++ src/main/kotlin/abc.java
@@ -0,0 +1,38 @@
+import coroutine.SuspendFromJavaExample;
+import kotlin.coroutines.Continuation;
+import kotlin.coroutines.CoroutineContext;
+import kotlin.coroutines.EmptyCoroutineContext;
+import org.jetbrains.annotations.NotNull;
+
+public class JavaTest {
+    /*public static void main(String[] args) {
+        JavaView javaView = new JavaView();
+        javaView.setOnClickListener(new JavaView.OnClickListener() {
+            @Override
+            public void onClick(JavaView v) {
+                System.out.println("After JavaView On Click");
+            }
+        });
+        javaView.click();
+    }*/
+
+
+
+    public static void main(String[] args) throws InterruptedException {
+        SuspendFromJavaExample.INSTANCE.getUserInfo(100L, new Continuation<String>() {
+            @NotNull
+            @Override
+            public CoroutineContext getContext() {
+                return EmptyCoroutineContext.INSTANCE;
+            }
+
+            @Override
+            public void resumeWith(@NotNull Object o) {
+                System.out.println(o+"");
+            }
+        });
+
+        // 防止程序提前结束
+        Thread.sleep(2000L);
+    }
+}
---
+++ src/main/kotlin/def.kt
@@ -0,0 +1,60 @@
+fun main() {
+    val kotlinView = KotlinView()
+    kotlinView.setOnClickListener {
+        println("After KotlinView On Click")
+    }
+    kotlinView.setOnClickListener(object: (KotlinView) -> Unit {
+        override fun invoke(p1: KotlinView) {
+            println("After KotlinView On Click")
+        }
+    })
+    kotlinView.click()
+
+    kotlinView.setOnLongClickListener(object : KotlinView.OnLongClickListener{
+        override fun onLongClick(v: KotlinView) {
+        }
+    })
+    kotlinView.setOnLongClickListener(KotlinView.OnLongClickListener { v: KotlinView? ->
+
+    })
+
+    kotlinView.setOnLongClickListener {
+
+    }
+    kotlinView.longClick()
+
+    val javaView = JavaView()
+    javaView.setOnClickListener(object : JavaView.OnClickListener {
+        override fun onClick(v: JavaView?) {
+            TODO("Not yet implemented")
+        }
+    })
+
+    javaView.setOnClickListener(JavaView.OnClickListener { v: JavaView? ->
+
+    })
+
+    javaView.setOnClickListener({ v: JavaView? ->
+
+    })
+
+    javaView.setOnClickListener({ v ->
+
+    })
+
+    javaView.setOnClickListener({ it ->
+
+    })
+
+    javaView.setOnClickListener({
+
+    })
+
+    javaView.setOnClickListener() {
+
+    }
+
+    javaView.setOnClickListener {
+
+    }
+}
